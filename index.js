1a884e8ffd9732dda7576eb98972b8ced9442b02
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-10-01 16:55:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
 
-// Add your functions and code here
 function destructivelyAppendKitten(name) {
-  return kittens.push(name)
+  kittens.push(name);
+  return kittens;
 }
+
 function destructivelyPrependKitten(name) {
-  return kittens.unshift(name)
+  kittens.unshift(name);
+  return kittens;
 }
+
 function destructivelyRemoveLastKitten() {
-  return kittens.pop()
+  kittens.pop();
+  return kittens;
 }
+
 function destructivelyRemoveFirstKitten() {
-  return kittens.shift()
+  kittens.shift();
+  return kittens;
 }
+
 function appendKitten(name) {
-  return [...kittens, name]
+  return [...kittens, name];
 }
+
 function prependKitten(name) {
   return [name, ...kittens]
 }
+
 function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1)
+  return kittens.slice(0,kittens.length-1)
 }
+
 function removeFirstKitten() {
   return kittens.slice(1)
 }
